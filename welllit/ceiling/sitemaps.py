1356819b701e27c7bac5a2ff95e3cfec2a4b547a
Caxapochek <ilay1470@yandex.ru>
2023-03-22 09:40:23 +0300

    add sitemap

---
+++ welllit/ceiling/sitemaps.py
@@ -0,0 +1,18 @@
+from django.contrib.sitemaps import Sitemap 
+from django.urls import reverse
+
+class StaticViewSitemap(Sitemap):
+
+    def items(self):
+        return [
+            'main']
+            # 'installation-procedure', 
+            # 'colors-and-textures',
+            # 'contactus',
+            # 'price-list',
+            # 'calculator',
+            # 'portfolio']
+
+    def locations(self, item):
+        return reverse(item)
+
--- welllit/welllit/urls.py
+++ welllit/welllit/urls.py
@@ -16,12 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
 
 from . import settings
 from ceiling.views import *
 
+from ceiling.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
     path('', include('ceiling.urls'))
 ]
 
